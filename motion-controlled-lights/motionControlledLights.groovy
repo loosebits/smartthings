262fabd4c43e141dfb691c570d83d2513b039ea5
Rand McNeely <randall_mcneely@bcbstx.com>
2017-03-26 15:57:49 -0500

    Get it working, add timing control

--- motion-controlled-lights/motionControlledLights.groovy
+++ motion-controlled-lights/motionControlledLights.groovy
@@ -3,33 +3,107 @@ definition(
   name: "Motion Controlled Lights",
   namespace: "loosebits",
   author: "Rand McNeely",
-  description: "Add motion control to a dimmer.  Light level based on nearby light levels"
-  category: "Health & Wellness",
+  description: "Add motion control to a dimmer.  Light level based on nearby light levels",
+  category: "Health & Wellness"
 )
 
 preferences {
-  page(name: "rootPage")
-  page(name: "schedulingPage")
-  page(name: "completionPage")
-  page(name: "numbersPage")
-  page(name: "controllerExplanationPage")
-  page(name: "unsupportedDevicesPage")
+  page name: "rootPage"
+  page name: "timeIntervalInput"
 }
 
 def rootPage() {
-  dynamicPage(name: "rootPage", title: "", install: true, uninstall: true) {
+  dynamicPage(name: "rootPage", title: "", uninstall: true, nextPage:"timeIntervalInput") {
 
     section("Which motion controller") {
       input(name: "motionController", type: "capability.motionSensor", title: "Motion Sensor", required: true)
     }
     section("Which dimmer to control") {
-      input(name:"dimmer" type:"capability.switchLevel", title: "Dimmer", required:true)
+      input(name:"dimmer", type:"capability.switchLevel", title: "Dimmer", required:true)
     }
     section("Based on what dimmers") {
-      input(name:"referenceDimmers", type:"capability.switchLevel", title:"Reference Dimmers", required: true, mutliple: true)
+      input(name:"referenceDimmers", type:"capability.switchLevel", title:"Reference Dimmers", required: true, multiple: true)
+    }
+    section("Add an additional offset") {
+      input(name:"offset", type:"number", title:"Offset from Average", required: true, defaultValue: 20)
     }
   }
 }
+
+def timeIntervalInput() {
+  dynamicPage(name: "timeIntervalInput", install: true, uninstall: true) {
+  section {
+      input "startTimeType", "enum", title: "Starting at", options: [["time": "A specific time"], ["sunrise": "Sunrise"], ["sunset": "Sunset"]], defaultValue: "time", submitOnChange: true
+      if (startTimeType in ["sunrise","sunset"]) {
+        input "startTimeOffset", "number", title: "Offset in minutes (+/-)", range: "*..*", required: false
+      } else {
+        input "starting", "time", title: "Start time", required: false
+      }
+    }
+    section {
+      input "endTimeType", "enum", title: "Ending at", options: [["time": "A specific time"], ["sunrise": "Sunrise"], ["sunset": "Sunset"]], defaultValue: "time", submitOnChange: true
+      if (endTimeType in ["sunrise","sunset"]) {
+        input "endTimeOffset", "number", title: "Offset in minutes (+/-)", range: "*..*", required: false
+      } else {
+        input "ending", "time", title: "End time", required: false
+      }
+    }
+  }
+}
+
+private getTimeOk() {
+  def result = true
+  def start = timeWindowStart()
+  def stop = timeWindowStop()
+  if (start && stop && location.timeZone) {
+    result = timeOfDayIsBetween(start, stop, new Date(), location.timeZone)
+  }
+  log.trace "timeOk = $result"
+  result
+}
+
+private timeWindowStart() {
+  def result = null
+  if (startTimeType == "sunrise") {
+    result = location.currentState("sunriseTime")?.dateValue
+    if (result && startTimeOffset) {
+      result = new Date(result.time + Math.round(startTimeOffset * 60000))
+    }
+  }
+  else if (startTimeType == "sunset") {
+    result = location.currentState("sunsetTime")?.dateValue
+    if (result && startTimeOffset) {
+      result = new Date(result.time + Math.round(startTimeOffset * 60000))
+    }
+  }
+  else if (starting && location.timeZone) {
+    result = timeToday(starting, location.timeZone)
+  }
+  log.trace "timeWindowStart = ${result}"
+  result
+}
+
+private timeWindowStop() {
+  def result = null
+  if (endTimeType == "sunrise") {
+    result = location.currentState("sunriseTime")?.dateValue
+    if (result && endTimeOffset) {
+      result = new Date(result.time + Math.round(endTimeOffset * 60000))
+    }
+  }
+  else if (endTimeType == "sunset") {
+    result = location.currentState("sunsetTime")?.dateValue
+    if (result && endTimeOffset) {
+      result = new Date(result.time + Math.round(endTimeOffset * 60000))
+    }
+  }
+  else if (ending && location.timeZone) {
+    result = timeToday(ending, location.timeZone)
+  }
+  log.trace "timeWindowStop = ${result}"
+  result
+}
+
 
 def intslled() {
   initialize();
@@ -41,22 +115,34 @@ def updated() {
 }
 
 def initialize() {
-  subscribe("motionController", "motion.active", motionDetected);
-  subscribe("motionController", "motion.inactive", noMotionDetected);
+
+  subscribe(motionController, "motion.active", motionDetected);
+  subscribe(motionController, "motion.inactive", noMotionDetected);
 }
 
 def motionDetected(e) {
-  def level = new BigDecimal();
-  referenceDimmers.each { ref -> 
-    def refLevel = 0;
-    if (ref.currentSwitch == 'on') {
-      level.add(ref.levelState.numberValue);
-      log.debug("Dimmer at $ref.levelState.numberValue");
-    } else {
-      log.debug("Dimmer off");
+  int level = 0;
+  int size = 0;
+  def targetLevel
+  if (timeOk) {
+    referenceDimmers.each { ref -> 
+      size++;
+      def refLevel = 0;
+      if (ref.currentSwitch == 'on') {
+        level += ref.levelState.numberValue.intValue();
+        log.debug("Dimmer at $ref.levelState.numberValue");
+      } else {
+        log.debug("Dimmer off");
+      }
     }
+    log.debug("Number of reference dimmers: $size, Total illumination: $level")
+    targetLevel = level / size + offset;
+  } else {
+    targetLevel = 100
   }
-  def targetLevel = Math.min(level.doubleValue() / referenceDimmers.size() + 20,100);
+  if (targetLevel > 100) {
+    targetLevel = 100;
+  }
   log.debug("Setting dimmer to $targetLevel");
   dimmer.setLevel(targetLevel);
 }
